9631ed0595dff8d022c781280d1948344685acb9
Engr.Sunday <asmelitus@gmail.com>
2018-10-06 22:50:14 +0100

    created TaxesFees component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Container } from 'reactstrap';
 import './App.css';
 import Subtotal from './components/Subtotal/Subtotal';
 import PickupSavings from './components/PickupSavings/PIckupSavings';
+import TaxesFees from './components/TaxesFees/TaxesFees';
+
 class App extends Component {
   constructor(props){
     super(props);
  
     this.state = {
       total: 100,
-      pickupSavings: -3.85
+      pickupSavings: -3.85,
+      taxes: 0
     }
   }
   render() {
@@ -19,6 +22,8 @@ class App extends Component {
         <Container className="container-purchase_summary">
            <Subtotal price={this.state.total.toFixed(2)} />
            <PickupSavings price={this.state.pickupSavings} />
+           <TaxesFees taxes={this.state.taxes} />
+           <hr />
         </Container>
       </div>
     );
---
+++ src/components/TaxesFees/TaxesFees.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import {  Row, Col } from 'reactstrap';
+
+class Subtotal extends Component{
+    render(){
+        return(
+            <Row className="show-grid">
+              <Col xs="6" style={{fontSize: "15px"}}>Est. taxes & fees (base on 94085)</Col>
+              <Col xs="6" style={{fontSize: "15px"}}>{`$${ this.props.taxes}`}</Col>
+            </Row>       
+        )
+    }
+}
+
+export default Subtotal;
