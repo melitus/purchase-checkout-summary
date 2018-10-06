71e43d4b79f5919f213cd954f1de8a0d654f96e8
Engr.Sunday <asmelitus@gmail.com>
2018-10-06 20:31:07 +0100

    :heavy_plus_sign: installed reastrap and boostrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../../assets/images/logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-
-        <Dashboard/>
-        
+        <h1>Hello world</h1>
       </div>
     );
   }
