168875784c7145a968825c92078271c9972aeda4
Engr.Sunday <asmelitus@gmail.com>
2018-10-06 19:56:45 +0100

    set up project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from '../../assets/images/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <Dashboard/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
